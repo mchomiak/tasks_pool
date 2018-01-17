3b5eee8baf263b7a074714e008de60528c8ff5c9
Michal Chomiak <michal.chomiak@ignitetech.com>
2018-01-17 15:22:06 +0100

    small fixes

--- src/tpool.cpp
+++ src/tpool.cpp
@@ -8,12 +8,11 @@ using namespace std;
 namespace tpool
 {
 
-
 tpool::tpool(uint32_t no_threads)
 	:	no_threads(no_threads)
 {
-	//if (no_threads == 0)
-	//	throw std::exception("Number of threads can't be 0");
+	if (no_threads == 0)
+		throw std::runtime_error("Number of threads can't be 0");
 }
 
 tpool::~tpool()
